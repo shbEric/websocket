18522bf3bc75e04cf68fa3b898bd3e7d21387c1e
shbEric <1289878918@qq.com>
2020-04-05 08:08:56 +0800

    websocket监听器

---
+++ src/main/java/com/super404/websocket/listener/ConnectEventListener.java
@@ -0,0 +1,17 @@
+package com.super404.websocket.listener;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+@Component
+public class ConnectEventListener implements ApplicationListener<SessionConnectEvent> {
+    @Override
+    public void onApplicationEvent(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        System.out.println("【ConnectEventListener监听器事件 类型】"+headerAccessor.getCommand().getMessageType());
+    }
+}
---
+++ src/main/java/com/super404/websocket/listener/SubscribeEventListener.java
@@ -0,0 +1,27 @@
+package com.super404.websocket.listener;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+
+/**
+ * springboot使用订阅事件
+ */
+@Component
+public class SubscribeEventListener implements ApplicationListener<SessionSubscribeEvent> {
+
+    /**
+     * 在事件触发的时候调用这个方法
+     *
+     * StompHeaderAccessor 简单消息传递协议中处理消息头的基类
+     * 通过这个类，可以获取消息类型（例如发布订阅，建立连接断开连接）、会话id等
+     * @param event
+     */
+    @Override
+    public void onApplicationEvent(SessionSubscribeEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        System.out.println("【SubscribeEventListener监听器事件 类型】"+headerAccessor.getCommand().getMessageType());
+    }
+}
